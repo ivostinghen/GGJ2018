4a382f003d98ec3a15e79f967e59e037a1881b28
Ivo Stinghen <iasf@icomp.ufam.edu.br>
2018-01-27 03:22:05 -0400

    Fist commit

---
+++ GGJ2018/Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehavior : MonoBehaviour {
+
+    Transform target=null;
+    public float speed;
+    Vector3 pos;
+    
+	void Awake () {
+        enabled = false;
+        target = GameObject.Find("Target").transform;
+        if (target != null) enabled = true;
+    }
+
+	void LateUpdate () {
+        pos = target.position;
+        // pos.y = 15;
+        transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime * speed);
+	}   
+}
---
+++ GGJ2018/Assets/Scripts/Character.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    Animator anim;
+    Rigidbody rb;
+    public float jumpForce,horSpeed;
+    bool isJumping = false;
+    public  void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Update(){
+
+        Move();
+    }
+
+    public void Move()
+    {
+        float hor = Input.GetAxis("Horizontal") * horSpeed;
+        
+        
+        rb.velocity = new Vector3(hor, rb.velocity.y,0);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(0,jumpForce,0);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+
+        
+    }
+}
